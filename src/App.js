697ef9170de72d4c0ed8852f8ea06b4ee3304cde
Saif <sshussaini96@gmail.com>
2021-06-21 12:31:55 -0400

    production build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import Movie from './Movie';
-import firebase from './firebase';
 
 
 const apiUrl = "http://api.themoviedb.org/3/discover/movie?api_key=d62e1adb9803081c0be5a74ca826bdbd";
