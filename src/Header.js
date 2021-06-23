c4ddbe3314352a69a2c549c7bd71a6e14bb487e0
Saif <sshussaini96@gmail.com>
2021-06-23 14:35:39 -0400

    added styling, search bar fixes

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+return (
+<header>
+      <h1><a href="index.html">watchit</a></h1>
+</header>
+)
+}
+
+export default Header;
--- src/Movie.js
+++ src/Movie.js
@@ -1,17 +1,15 @@
-import React, { useState } from "react";
-
 const images = "https://image.tmdb.org/t/p/w500";
 const Movie = ( {
-    id, poster, title, voteAverage, overview
+    id, poster, title, voteAverage, overview, handleClick
 }) => {
     return (
     <div className="moviePoster">
-        <img src={`${images}/${poster}`} alt={title} />
+        <img src={ poster ? `${images}/${poster}` : "https://www.movienewz.com/img/films/poster-holder.jpg" } alt={title} />
         <div className="movieInfo">
-            {/* <button><span>+</span></button> */}
             <h2>{title}</h2>
             <p class="voteStyle">{voteAverage}</p>
             <p class="summary">{overview}</p>
+            <p class="key">{id}</p>
 
         </div>
 
