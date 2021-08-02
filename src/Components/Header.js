1b1a794fd0f9233b8bc6a833adb510f005f0eeb0
Saif <sshussaini96@gmail.com>
2021-08-02 16:01:26 -0400

    font fixes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import '../App.css'
 function Header() {
 return (
 
-<header>
+<header className="yui3-cssreset">
       <h1><a href="/"><span className="sr-only" htmlFor="header">Watch It</span>watchit</a></h1>
 </header>
 
