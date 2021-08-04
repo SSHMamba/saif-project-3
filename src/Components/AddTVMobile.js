f9639c705e152348e37850e2b661b10f9f915ac8
Saif <sshussaini96@gmail.com>
2021-08-03 21:11:57 -0400

    fixed add button

--- src/Components/AddTVMobile.js
+++ src/Components/AddTVMobile.js
@@ -2,16 +2,16 @@ import './AddToList.css'
 import firebase from "../firebase";
 import { useState } from 'react';
 
-const AddTVMobile = ({addItem}) => {
+const AddTVMobile = ({addItem, reloadPage}) => {
 const [added, setAdded] = useState(true);
 
-    const dbRef = firebase.database().ref('user/lists/movies');
+    const dbRef = firebase.database().ref('user/lists/tv');
 
     const addToFirebase = () => {
         dbRef.push({
             poster: `https://image.tmdb.org/t/p/original${addItem.poster_path}`,
             id: addItem.id,
-            title: addItem.title
+            name: addItem.name
         })
         setAdded(false)
 
